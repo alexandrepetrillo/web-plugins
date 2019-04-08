157784e69ac0f8e49fff2b13a508e0f24ea1f0e2
Alexandre PETRILLO <alexandre-externe.petrillo@enedis.fr>
2019-04-08 16:35:10 +0200

    test

--- plugins/show-cout-item-backlog/main.js
+++ plugins/show-cout-item-backlog/main.js
@@ -11,6 +11,35 @@ rtb.onReady(() => {
           let selectedWidgets = await rtb.board.selection.get()
           console.log(selectedWidgets)
 
+          let stickers = selectedWidgets.filter(widget => widget.type === 'STICKER')
+
+          await rtb.board.widgets.create(stickers.map(sticker => ({
+            type: 'shape',
+            text: '3',
+            style: {
+              backgroundColor: "transparent",
+              backgroundOpacity: 1,
+              bold: 0,
+              borderColor: "#1a1a1a",
+              borderOpacity: 1,
+              borderStyle: 2,
+              borderWidth: 2,
+              fontFamily: 10,
+              fontSize: 12,
+              highlighting: "",
+              italic: 0,
+              shapeType: 4,
+              strike: 0,
+              textAlign: "c",
+              textAlignVertical: "m",
+              textColor: "#1a1a1a",
+            },
+            x: sticker.x + sticker.bounds.width,
+            y: sticker.y,
+            width: 30,
+            height: 30,
+          })))
+
           let groups = await rtb.board.groups.get()
           console.log(groups)
 
