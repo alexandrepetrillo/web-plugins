ad8357facbe25066e767813bc1c35cc645366cb5
Alexandre PETRILLO <alexandre-externe.petrillo@enedis.fr>
2019-12-11 17:22:32 +0100

    ok

--- plugins/show-cout-item-backlog/main.js
+++ plugins/show-cout-item-backlog/main.js
@@ -3,10 +3,18 @@ rtb.onReady(() => {
 
   function getJiraId(w) {
     try {
-    return w.card.customFields.filter(f => f.value.match(/\w{3}-\d+/)).map(f => f.value)[0]
+    return w.card.customFields.filter(f => f.tooltip.match(/\w{3}-\d+/)).map(f => f.value)[0]
     } catch (e) {
       console.log('impossible de récuperer le jira ')
       console.log(w)
+    }
+  }
+
+  function getJiraCost(w) {
+    try {
+      return w.card.customFields.filter(f => f.tooltip == "[SIX] Coût item backlog").map(f => f.value)[0]
+    } catch (e) {
+      return ''
     }
   }
 
@@ -15,55 +23,20 @@ rtb.onReady(() => {
   
     var unknowCosts = [];
     var cost = jiras.map( jira => {
-      if (jiraCostById[jira] == undefined || jiraCostById[jira] == '') {
+      if (getJiraCost(jira) == undefined || getJiraCost(jira) == '') {
         unknowCosts.push(jira);
         return 0
       } else{
-        return parseFloat(jiraCostById[jira])
+        return parseFloat(getJiraCost(jira))
       }
     })
     .reduce ((a,b)=>a+b, 0);
     
-  
-   var unknowCosts = [];
-   var cost = jiras.map( jira => {
-     if (jiraCostById[jira] == undefined || jiraCostById[jira] == '') {
-       unknowCosts.push(jira);
-       return 0
-     } else{
-       return parseFloat(jiraCostById[jira])
-     }
-   })
-   .reduce ((a,b)=>a+b, 0);
-  
     var warn = '';
     if (unknowCosts.length>0 ) {
       warn += ', '+ unknowCosts.length + ' coût(s) inconnu(s)';
     }
-    return {jiras, cost, warn, unknowCosts, jiraCostById}
-  }
-
-  try {
-  // chargement des cout jira
-  var jiraCostById = {}
-  var xmlhttp = new XMLHttpRequest()
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
-      var lines = this.responseText.split('\r\n')
-      lines.forEach(line => {
-        var elems = line.split("\t")
-        jiraCostById[elems[0]] = elems[1]
-      })
-      console.log(jiraCostById)
-    } else {
-      console.log(this)
-    }
-  };
-  xmlhttp.open('GET', 'https://docs.google.com/spreadsheets/d/1cQeYW0F-ryag2_9RraJGNB2wUete6FJ31JivIit5ueY/export?format=tsv&id=1cQeYW0F-ryag2_9RraJGNB2wUete6FJ31JivIit5ueY&gid=1098716530');
-  xmlhttp.send()
-  } catch (e) {
-    console.log(e)
+    return {jiras, cost, warn, unknowCosts}
   }
 
   rtb.addListener('SELECTION_UPDATED', async (x) => {
@@ -84,7 +57,7 @@ rtb.onReady(() => {
          
           var all = await getCost()
           console.log(all)
-          var {jiras, cost, warn, unknowCosts, jiraCostById} = all
+          var {jiras, cost, warn, unknowCosts} = all
           prompt('JIRA sélectionnées', jiras.join(', '))
           console.log(jiras.join(', '))
 
@@ -94,7 +67,7 @@ rtb.onReady(() => {
           sel.filter(w => w.type === "CARD").forEach(w => {
             var jira = getJiraId(w)
               if (jira) {
-              var cost = jiraCostById[jira]
+              var cost = getJiraCost(jira)
               if (!cost) {
                 withoutCost.push(w.id)
               }
@@ -102,29 +75,6 @@ rtb.onReady(() => {
           })
           
           await rtb.board.selection.selectWidgets(withoutCost)
-
-          /*
-           let selectedWidgets = await rtb.board.selection.get()
-           let stickers = selectedWidgets.filter(widget => widget.type === 'JIRACARD')
-          */
-          /*
-                    var createdWidgets = await rtb.board.widgets.create(
-                      stickers
-                      .filter(sticker => widgetIdToJira[sticker.id] && jiraToCout[widgetIdToJira[sticker.id]])
-                      .map(sticker => ({
-                        createdUserId: sticker.id,
-                        type: 'shape',
-                        style: {
-                          shapeType: 4
-                        },
-                        text: jiraToCout[widgetIdToJira[sticker.id]],
-                        x: sticker.bounds.left + sticker.bounds.width,
-                        y: sticker.bounds.top,
-                        width: 30,
-                        height: 30,
-                      }))
-                    )
-          */
         }
       }
     }
