b2c46798353d39fb7246ab9a82231c3291862019
Alexandre PETRILLO <alexandre-externe.petrillo@enedis.fr>
2023-04-05 21:38:41 +0200

    ok

--- plugins/show-cout-item-backlog/main.js
+++ plugins/show-cout-item-backlog/main.js
@@ -82,7 +82,7 @@ miro.onReady(() => {
 			if (widgetsOverlays.length === 1) {
 				console.log(widgetsOverlays)
 				var existing = (widgetsOverlays[0].metadata['link'] || [])
-				widgetsOverlays[0].metadata['link']= [...existing, shape.id]
+				// widgetsOverlays[0].metadata['link']= [...existing, shape.id]
 			}
 		}			
     });
